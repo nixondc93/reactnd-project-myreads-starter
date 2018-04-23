54d63571925a426a24a7b427f34e0635ae48354a
Derry <nixoncd93@gmail.com>
2018-04-23 10:02:15 -0700

    Updated README

--- src/App.js
+++ src/App.js
@@ -8,12 +8,6 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
     errorOnSearch: false,
     searchResults: null,
     currentlyReadingShelf: null,
@@ -81,15 +75,14 @@ class BooksApp extends React.Component {
           });
           return searchResult
         });
-        console.log(searchResults);
         this.setState({searchResults, errorOnSearch: false});
       }
     });
-  }
+  };
 
   changeShelf = (book, shelf) => {
     BooksAPI.update(book, shelf).then((resBook) => this.getBooks());
-  }
+  };
 
   render() {
 
