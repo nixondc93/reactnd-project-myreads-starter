aaf1dc52714e123c73bab9850f8bd08b0dba6476
Derry <nixoncd93@gmail.com>
2018-04-22 19:04:18 -0700

    condensed all shelf components into on inorder to remain DRY

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
-import CurrentlyReading from './CurrentlyReading';
-import WantToRead from './WantToRead';
-import Read from './Read';
-
+import * as BooksAPI from './BooksAPI'
+import Shelf from './Shelf';
+import SearchPage from './SearchPage';
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -14,34 +12,46 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-
+    currentlyReadingShelf: null,
+    wantToReadShelf: null,
+    readShelf: null,
     showSearchPage: false
+  }
+
+  componentDidMount(){
+    let currentlyReadingShelf = [], wantToReadShelf = [], readShelf = []; 
+    BooksAPI.getAll().then((books) => {
+      books.forEach(book => {
+        switch(book.shelf){
+          case "currentlyReading":
+            currentlyReadingShelf.push(book);
+            break;
+          case "wantToRead":
+            wantToReadShelf.push(book);
+            break;
+          case "read":
+            readShelf.push(book);
+            break;
+        }        
+      });
+
+      this.setState({currentlyReadingShelf, wantToReadShelf, readShelf})
+    });
+  }
+
+  onsearch(){
+
+  }
+
+  changeShelf(shelf, book){
+
   }
 
   render() {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <SearchPage/>
         ) : (
           <div className="list-books">
             <div className="list-books-title">
@@ -49,9 +59,9 @@ class BooksApp extends React.Component {
             </div>
             <div className="list-books-content">
               <div>
-                <CurrentlyReading/>
-                <WantToRead/>
-                <Read/>
+                <Shelf books={this.state.currentlyReadingShelf}/>
+                <Shelf books={this.state.wantToReadShelf}/>
+                <Shelf books={this.state.readShelf}/>
               </div>
             </div>
             <div className="open-search">
